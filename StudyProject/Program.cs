56cce8f91564d8dd28bcc05a827b0ff64ec0d4a9
Luiz Camargo <luiz.zito@hotmail.com>
2023-02-05 12:06:20 +0000

    updates

--- StudyProject/Program.cs
+++ StudyProject/Program.cs
@@ -1,19 +1,9 @@
 ﻿using BenchmarkDotNet.Running;
 using StudyProject;
 
-Console.WriteLine("rodador".IsPalindrome()); // true
-Console.WriteLine("mamam".IsPalindrome()); // true
-Console.WriteLine("rir".IsPalindrome()); // true
-Console.WriteLine("Livre do poder vil".IsPalindrome()); // true
-Console.WriteLine("O trote torto".IsPalindrome()); // true
 
-Console.WriteLine("amor".IsPalindrome()); // false
-Console.WriteLine("celular".IsPalindrome()); // false
-Console.WriteLine("caneta".IsPalindrome()); // false
-Console.WriteLine("pc".IsPalindrome()); // false
-Console.WriteLine("abac".IsPalindrome()); // false
 
-//var resultado = BenchmarkRunner.Run<BenchmarkPalindrome>();
+var resultado = BenchmarkRunner.Run<BenchmarkPalindrome>();
 
 
 
